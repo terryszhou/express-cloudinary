f652a5b5d3ac7f55307b3ae50f25f89ebbbfcd04
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-06-08 11:58:36 -0500

    Initial commit, cloudinary and multer downloaded

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
+// MODULE SETUP ----------------
+require('dotenv').config()
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var app = express();
+var multer = require('multer')
+var upload = multer({ dest: './uploads/'})
+var cloudinary = require('cloudinary')
 
+// MIDDLEWARE ----------------
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 
+// ROUTES ----------------
+
+// GET / HOME PAGE
 app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.listen(3000);
+// POST / FILE UPLOAD
+app.post('/', upload.single('myFile'), (req, res) => {
+  res.send(req.file)
+})
+
+// LISTEN TO PORT ----------------
+app.listen(5000);
